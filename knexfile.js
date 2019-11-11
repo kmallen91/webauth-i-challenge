f453355aa62b6a9b477b606df4c57f571e564498
Kevin Allen <kevin.allen91@gmail.com>
2019-11-11 13:45:12 -0800

    created api methods

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,11 @@ module.exports = {
       filename: './users.sqlite3'
     },
     useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    },
     migrations: {
       directory: './data/migrations'
     }
--- users/userModel.js
+++ users/userModel.js
@@ -1,3 +1,34 @@
 const db = require('../data/dbConfig');
 
-module.exports = {};
+module.exports = {
+  find,
+  findBy,
+  findById,
+  add
+};
+
+function find() {
+  return db('users').select('id', 'username');
+}
+
+function findBy(filter) {
+  return db('users')
+    .select('id', 'username', 'password')
+    .where(filter);
+}
+
+function findById() {
+  return db('users')
+    .select('id', 'username')
+    .where({ id })
+    .first();
+}
+
+function add(user) {
+  return db('users')
+    .insert(user, 'id')
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
